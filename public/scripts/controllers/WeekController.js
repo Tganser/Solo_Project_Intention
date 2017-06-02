17cf166718044672142963e585e969c5bb1ebc3e
Tessa Ganser <t.m.ganser@gmail.com>
2017-06-02 10:39:01 -0500

    merging indexhtml

--- public/scripts/controllers/WeekController.js
+++ public/scripts/controllers/WeekController.js
@@ -10,9 +10,55 @@ googleAuthApp.controller('WeekController', function($http) {
         url: '/getcalendardata'
       }).then(function(response){
         console.log("response from server in get caldata: ", response.data);
+        cm.caldata = response.data;
         return response.data;
       });
     };
+
+  vm.options = {
+            chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        vm.data = [
+            {
+                key: "Event1",
+                y: vm.nameIn
+            },
+            {
+                key: "Event2",
+                y: 2.5
+            },
+            {
+                key: "Event3",
+                y: 6
+            },
+            {
+                key: "Event4",
+                y: 1
+            },
+            {
+                key: "Event5",
+                y: 1
+            }
+        ];
+});
 
   // this.updateIntentions = function(){
   //   console.log("in update intentions on");
@@ -25,4 +71,4 @@ googleAuthApp.controller('WeekController', function($http) {
   //   });
   // };
 
-});
+// });
